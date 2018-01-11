175ccfa0e0e891e9d06511bbf664909e1013871e
Derek Morey <dman6505@gmail.com>
2018-01-10 22:12:28 -0500

    added DB schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+drop table if exists song;
+create table song (
+	id integer primary key autoincrement,
+	name text not null,
+	artist text,
+	album text,
+	genre text
+	minutes integer,
+	seconds integer,
+	constraint uc_song unique (name)
+);
