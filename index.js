bf3af7706f08b9be4ff7862f1d36ddedb33fe6ed
Derek Morey <dman6505@gmail.com>
2018-01-13 00:04:28 -0500

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 const port = process.env.PORT || 6505;
 
