857ee1d31645fd8fc594f5ba7c1f02c689415292
Derek Morey <dman6505@gmail.com>
2018-01-24 18:29:10 -0500

    Added express-bearer-token for parsing tokens out

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const bearerToken = require('express-bearer-token');
 
 const api = require('./api');
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bearerToken());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
