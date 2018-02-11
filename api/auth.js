298811c3cfa5d79c54564253f052396f0485c6b0
Derek Morey <dman6505@gmail.com>
2018-02-11 16:10:00 -0500

    removed jsdoc

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,3 @@
-/**
- * @module api/auth
- */
 const database = require('./database');
 const S = require('secure-serializer');
 
@@ -9,12 +6,6 @@ const db = database.getDB();
 
 const maxTokenAge = 86400 * 2; // 2 days
 
-/**
- * Middleware that checks that a user is authenticated
- * @arg {Object} req - the Express request object
- * @arg {Object} req - the Express response object
- * @arg {Function} next - the Express next middleware function
- */
 exports.authRequired = (req, res, next) => {
 	let data = null;
 	try {
--- api/database.js
+++ api/database.js
@@ -1,15 +1,8 @@
-/**
- * @module api/database
- */
 const Database = require('better-sqlite3');
 const config = require('../config');
 
 let conn = null;
 
-/**
- * Get a database connection
- * @returns {Object} - the database connection
- */
 exports.getDB = () => {
 	if (!conn) {
 		conn = new Database(config.dbFile);
