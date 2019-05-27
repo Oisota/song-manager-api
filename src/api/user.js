8d81722132ae4170911bb5d5c31793896d45c840
Derek Morey <derek.o.morey@gmail.com>
2019-05-26 23:20:17 -0400

    Updated users.js module
    The /me route was updated to return the user ID from the DB, and a route
    was added for inserting users into the DB

--- src/api/user.js
+++ src/api/user.js
@@ -1,12 +1,33 @@
 const express = require('express');
 
+const database = require('./database');
+
 const router = express.Router();
+const db = database.getDB();
 
 router.route('/me')
 	.get((req, res) => {
-		res.json({
-			id: 1,
+		/* TODO figure how to load user */
+		const q = `
+			select id
+			from user
+			where auth0_id = :sub;`;
+		const user = db.prepare(q).get(req.user);
+		if (!user) {
+			res.status(404).json({
+				message: 'User Not Found'
+			});
+		}
+		res.json(user);
+	});
+
+router.route('/users')
+	.post((req, res) => {
+		const q = 'insert into user (auth0_id) values (:id);';
+		db.prepare(q).run({
+			id: req.params.auth0ID,
 		});
+		res.status(201).end();
 	});
 
 module.exports = router;
