076e89229932d0b9356e2c47af761a8255196c63
Derek Morey <derek.o.morey@gmail.com>
2019-05-26 23:18:38 -0400

    Updated initial migration

--- migrations/migration-1.sql
+++ migrations/migration-1.sql
@@ -7,25 +7,12 @@ create table if not exists song (
 	genre text,
 	length_ integer,
 	user_id integer not null,
-	constraint uc_song unique (name)
+	constraint uc_song_user unique (name, user_id),
 	foreign key (user_id) references user(id)
 );
 
 drop table if exists user;
 create table if not exists user (
 	id integer primary key autoincrement,
-	email text not null,
-	hash text not null,
-	verified integer not null default 0,
-	role_id integer not null,
-	foreign key (role_id) references role(id)
+	auth0_id text not null
 );
-
-drop table if exists role;
-create table role (
-	id integer primary key autoincrement,
-	name text not null
-);
-
-insert into role (name) values ('user');
-insert into role (name) values ('admin');
