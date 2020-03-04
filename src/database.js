a8d49b01f750d816b351f91a1250843019f3ba03
Derek Morey <derek.o.morey@gmail.com>
2020-03-03 21:00:09 -0500

    removed better-sqlite3 dependency

--- src/database.js
+++ src/database.js
@@ -1,15 +1,5 @@
 const Sequelize = require('sequelize');
-const Database = require('better-sqlite3');
 const config = require('./config');
-
-let conn = null;
-
-exports.getDB = () => {
-	if (!conn) {
-		conn = new Database(config.dbFile);
-	}
-	return conn;
-};
 
 exports.sequelize = new Sequelize({
 	dialect: 'sqlite',
