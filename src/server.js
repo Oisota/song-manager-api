58dc44b7585ea97e1bc5be47e697ea37c718f480
Derek Morey <derek.o.morey@gmail.com>
2020-03-22 22:59:09 -0400

    Added default rate limit
    A default rate limit was added to all routes. This was done to
    protect the app from being spammed with requests.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bearerToken = require('express-bearer-token');
 const helmet = require('helmet');
 const sequelize = require('sequelize');
+const rateLimit = require('express-rate-limit');
 
 const config = require('./config');
 const routes = require('./routes');
@@ -19,6 +20,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bearerToken());
 app.use(morgan('dev'));
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 min
+	max: 500,
+	message: envelope(null, {
+		message: 'Too many requests, try again later',
+	})
+}));
 
 // main app routes
 app.use(`/api/${config.apiVersion}/users`, routes.songs);
