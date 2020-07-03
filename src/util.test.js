fb1c9336d386da141d8d3379c7c6b651bd01ca68
Derek Morey <derek.o.morey@gmail.com>
2020-07-02 22:12:49 -0400

    Added tests for jwt sign/verify functions

--- src/util.test.js
+++ src/util.test.js
@@ -1,5 +1,6 @@
 const {describe, it} = require('mocha');
 const { expect } = require('chai');
+const jwt = require('jsonwebtoken');
 
 const util = require('@/util');
 
@@ -14,5 +15,46 @@ describe('envelope', () => {
 		};
 		const result = util.envelope(data, null);
 		expect(result).to.deep.equal(wrapped);
+	});
+});
+
+describe('jwtSign', () => {
+	it('Should return a promise', () => {
+		const data = {
+			foo: 'bar'
+		}
+		const opts = {}
+		const result = util.jwtSign(data, opts);
+		expect(result).to.be.a('promise');
+	});
+
+	it('Should resolve to a jwt string', async () => {
+		const data = {
+			foo: 'bar'
+		}
+		const opts = {}
+		const result = await util.jwtSign(data, opts);
+		expect(result).to.be.a('string');
+	});
+})
+
+describe('jwtVerify', () => {
+	it('Should return a promise', () => {
+		const token = 'this an invalid token';
+		const opts = {};
+		const result = util.jwtVerify(token, opts);
+		result.catch(() => {}); // prevent unhandled promise reject message
+		expect(result).to.be.a('promise');
+	});
+
+	it('Should throw for invalid tokens', async () => {
+		const token = 'this an invalid token';
+		const opts = {};
+		try {
+			const _ = await util.jwtVerify(token, opts);
+		} catch (e) {
+			expect(e).to.be.a(typeof (new jwt.JsonWebTokenError()));
+			return;
+		}
 	});
 });
