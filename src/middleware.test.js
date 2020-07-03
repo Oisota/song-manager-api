278bec381341c0acd848eeced61762f1ca17bb85
Derek Morey <derek.o.morey@gmail.com>
2020-07-03 13:46:31 -0400

    added tests for role middleware

--- src/middleware.test.js
+++ src/middleware.test.js
@@ -1,5 +1,9 @@
 const { describe } = require('mocha');
+const chai = require('chai');
 const { expect } = require('chai');
+const spies = require('chai-spies');
+
+chai.use(spies);
 
 const mw = require('@/middleware');
 
@@ -14,3 +18,53 @@ describe('catchAllRoute', () => {
 		mw.catchAllRoute(req, res, next);
 	});
 });
+
+describe('errorHandler', () => {
+});
+
+describe('authRequired', () => {
+	it('Should return unauthorized for invalid tokens', () => {
+	});
+	it('Should call next for valid tokens', () => {
+	});
+});
+
+describe('role', () => {
+	it('Should reject request if user does not have role', () => {
+		const req = {
+			user: {
+				role: {
+					name: 'foo'
+				}
+			}
+		};
+		const res = {}
+		const next = chai.spy(() => {})
+		const middlwareFunc = mw.role('bar')
+		const fn = () => middlwareFunc(req, res, next);
+		expect(fn).to.throw();
+		expect(next).to.have.not.been.called();
+	});
+
+	it('Should call next if user has role', () => {
+		const req = {
+			user: {
+				role: {
+					name: 'foo'
+				}
+			}
+		};
+		const res = {}
+		const next = chai.spy(() => {})
+		mw.role('foo')(req, res, next);
+		expect(next).to.have.been.called();
+	});
+});
+
+describe('validate', () => {
+	it('Should call next if schema is valid', () => {
+	});
+	it('Should return bad request if schema throws error', () => {
+	});
+});
+
